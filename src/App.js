1a4d3d2ec00be1227747bc236e52c4572e78889a
abbassiddiqui99 <abbassiddiqui99@gmail.com>
2020-08-29 13:36:26 +0500

    Working has been started

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import data from "./data";
+import "./App.css";
 
 function App() {
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add("open");
+  };
+  const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove("open");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header className="header">
+        <div className="brand">
+          <button onClick={openMenu}>&#9776;</button>
+          <a href="index.html">Amazona</a>
+        </div>
+        <div className="header-links">
+          <a href="cart.html">Cart</a>
+          <a href="signin.html">SignIn</a>
+        </div>
       </header>
+      <aside className="sidebar">
+        <h3 className="sidepadding-aside">Shopping Categories</h3>
+        <button className="sidebar-close-button" onClick={closeMenu}>
+          x
+        </button>
+        <ul className="ul-items-aside">
+          <li>
+            <a href="index.html">Pants</a>
+          </li>
+          <li>
+            <a href="index.html">Shirts</a>
+          </li>
+        </ul>
+      </aside>
+      <main className="main">
+        <div className="content">
+          <ul className="products">
+            {data.products.map((product) => (
+              <li>
+                <div className="product">
+                  <img
+                    className="product-image"
+                    src={product.image}
+                    alt="product"
+                  />
+                  <div className="product-name">
+                    <a href="product.html"> {product.name} </a>
+                  </div>
+                  <div className="product-brand">{product.brand}</div>
+                  <div className="product-price">{product.price}</div>
+                  <div className="product-rating">
+                    {product.rating} Stars ({product.numReviews})
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </main>
+      <footer className="footer">All right reserved.</footer>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,35 @@
+export default {
+  products: [
+    {
+      name: "slim Shirt",
+      image: "/images/4.jpg",
+      brand: "Nike",
+      rating: 3.5,
+      numReviews: 5,
+    },
+    {
+      name: "slim pant",
+      image: "/images/2.jpg",
+      price: 63,
+      brand: "Nike",
+      rating: 4.0,
+      numReviews: 5,
+    },
+    {
+      name: "slim trouser",
+      image: "/images/3.jpg",
+      price: 65,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 5,
+    },
+    {
+      name: "slim asdf",
+      image: "/images/5.jpg",
+      price: 65,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 5,
+    },
+  ],
+};
